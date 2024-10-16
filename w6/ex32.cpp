c1c8818d10c1781879f8fd7e289519aadf77fe7f
KminuShin <msps100046@gmail.com>
2024-10-16 11:04:37 +0800

    w6 ex32 finish

--- w6/ex32.cpp
+++ w6/ex32.cpp
@@ -12,15 +12,32 @@ int main() {
     printf("> mul(*) is 3\n");
     printf("> div(/) is 4\n");
     printf("> mod(%%) is 5\n");
-    printf("give me [NUMBER1] [OPERATOR] [NUMBER2]\n");
-    scanf("%d%d%d", &num1, &opr, &num2);
 
-    
-    if(opr >= 4) {
-        while (!num2) {
-            //
+    while (1) {
+        printf("\ngive me [NUMBER1] [OPERATOR] [NUMBER2]\n");
+        printf("for exit, enter 0 0 0\n");
+        printf("for hint, enter 0 80 0\n");
+        scanf("%d%d%d", &num1, &opr, &num2);
+
+        if ((num1 == 0)&&(opr == 0)&&(num2 == 0)) {break;}
+
+        if ((opr == 80)&&(num1 == 0)&&(num2 == 0)) {
+            printf("\n> sum(+) is 1\n");
+            printf("> sub(-) is 2\n");
+            printf("> mul(*) is 3\n");
+            printf("> div(/) is 4\n");
+            printf("> mod(%%) is 5\n");
+            continue;
         }
-    }
+        
+        if(opr >= 4) {
+            while (!num2) {
+                printf("PLEASE DON'T DIVIDE BY 0!!\n");
+                printf("Let's try again\n");
+                printf("give me [NUMBER2]\n");
+                scanf("%d", &num2);
+            }
+        }
 
         if (opr == 1) {printf("%d + %d = %d\n",num1,num2,num1+num2);}
         else if (opr == 2) {printf("%d - %d = %d\n",num1,num2,num1-num2);}
@@ -28,4 +45,7 @@ int main() {
         else if (opr == 4) {printf("%d / %d = %d\n",num1,num2,num1/num2);}
         else if (opr == 5) {printf("%d %% %d = %d\n",num1,num2,num1%num2);}
         else {printf("oops wrong operator:%d", opr);}
+
+        printf("\nNEXT ROUND!!\n");
+    }
 }
