7d6acd461c33cd82293f299001fa1deb3dfb8e3b
KminuShin <msps100046@gmail.com>
2024-11-07 15:23:35 +0800

    w8 practice PrintStar PrintSquare

---
+++ w8/PrintSquare.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void PrintStar(int col) {
+    for(int i=0;i < col;i++) {
+        printf("*");
+    }
+    printf("\n");
+}
+
+void PrintSquare(int row, int col) {
+    for(int i=0;i < row;i++) {
+        PrintStar(col);
+    }
+}
+
+int main() {
+    int row, col;
+    scanf("%d%d", &row, &col);
+
+    PrintSquare(row, col);
+}
+
+/*
+row = 4, col = 6;
+******
+******
+******
+******
+*/
