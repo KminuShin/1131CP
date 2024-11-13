b32b739999a6de6452461ba0794ce10d5483d251
KminuShin <msps100046@gmail.com>
2024-11-14 01:24:39 +0800

    w8 sync MonsterHuntingGame

--- w8/MonsterHuntingGame.cpp
+++ w8/MonsterHuntingGame.cpp
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <cstdlib>
 
 void refreshScreen(int monsterCNT, int monsterDefaultHP, int monsterHP[]) {
     // clear
@@ -7,7 +9,7 @@ void refreshScreen(int monsterCNT, int monsterDefaultHP, int monsterHP[]) {
     // title
     printf("*** Monster Hunting Game ***\n\n");
 
-    // monster status
+    // print monster status
     for (int i=0;i < monsterCNT;i++) {
         printf("Monster %d ", i+1);
         if(monsterHP[i] > 0) {
@@ -23,7 +25,48 @@ void refreshScreen(int monsterCNT, int monsterDefaultHP, int monsterHP[]) {
     printf("\n");
 
     // user move guide
+    /* <moved to playerTurn> printf("Player's tern: (1)single attack/7P (2)group attack/2P ...> ");*/
+}
+
+void playerTurn() {
+    // ask user choice
     printf("Player's tern: (1)single attack/7P (2)group attack/2P ...> ");
+
+    scanf("%d", &choice);
+
+    // calculate monsterHP
+    if (choice == 1) { // player attack (Single)
+        printf("Which monster do you want to attack? ...> ");
+
+        // <to mod, choose var> 
+        // scanf("%d", &);
+
+        monsterHP[die] -= 7;
+    } else if (choice == 2) { // player attack (Multiple)
+        for (int i=0;i < monsterCNT;i++) {
+            if (monsterHP[i] > 0) monsterHP[i] -= 2;
+        }
+    }
+}
+
+void playerAttack() {
+    //
+}
+
+void enemyTurn() {
+    // Check survival and health
+
+    // if need to heal
+
+    // else attack
+}
+
+void enemyAttack() {
+    //
+}
+
+void enemyHeal() {
+    //
 }
 
 int main() {
@@ -44,17 +87,22 @@ int main() {
         // refresh screen
         refreshScreen(monsterCNT, monsterDefaultHP, monsterHP);
 
+        /*
         // wait user input
         scanf("%d", &choice);
 
         // calculate monsterHP
         if (choice == 1) {
+            // player attack (Single)
+            // <to mod> select monster
             monsterHP[die] -= 7;
         } else if (choice == 2) {
+            // player attack (Multiple)
             for (int i=0;i < monsterCNT;i++) {
                 if (monsterHP[i] > 0) monsterHP[i] -= 2;
             }
         }
+        */
 
         // die check
         for (int i=0;i < monsterCNT;i++) {
